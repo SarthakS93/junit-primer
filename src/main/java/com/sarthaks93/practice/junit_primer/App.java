21309d9cbaf2a84e111cb6779c8cde2f5da4450a
sarthak <sarthaks93@gmail.com>
2019-01-30 16:21:31 +0530

    init

---
+++ src/main/java/com/sarthaks93/practice/junit_primer/App.java
@@ -0,0 +1,13 @@
+package com.sarthaks93.practice.junit_primer;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
